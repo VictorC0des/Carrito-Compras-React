7fc6f3471407dd0c6087bfaff910432259e1e562
Jacob Escareño <jacobescareno6@gmail.com>
2024-07-04 11:37:46 -0600

    Feat: Footer modified

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,11 @@ const Footer = () => {
     <footer className="footer-container">
       <div className="social-media-links">
         <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"><FaFacebookF /></a>
-        <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer"><FaYoutube /></a>
+        <a href="https://acortar.link/jlRIU8" target="_blank" rel="noopener noreferrer"><FaYoutube /></a>
         <a href="https://www.tiktok.com" target="_blank" rel="noopener noreferrer"><FaTiktok /></a>
         <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
       </div>
-      <p>© 2023 Mercado. Todos los derechos reservados.</p>
+      <p>© 2024 Mercado. Todos los derechos reservados.</p>
     </footer>
   );
 };
